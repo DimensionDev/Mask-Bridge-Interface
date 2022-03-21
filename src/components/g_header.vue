93465d1329989a26824d28c879b56f2a156da91e
billy <Billy@mask.io>
2022-03-21 22:20:48 +0800

    fix: fix temp

--- src/components/g_header.vue
+++ src/components/g_header.vue
@@ -1,27 +1,35 @@
 <template>
   <div class="header_box">
-    <img class="logo" src="../assets/logo.png" alt="" />
+    <img @click="toMask" class="logo" src="../assets/logo.svg" alt="" />
   </div>
 </template>
 
 <script>
 export default {
   name: "g_header",
-  setup() {},
+  setup() {
+    const toMask = () => {
+      window.open("//mask.io", "_blank");
+    };
+    return {
+      toMask,
+    };
+  },
 };
 </script>
 <style lang='less' scoped>
 .header_box {
   padding: 1.5rem 0;
   .logo {
+    cursor: pointer;
     width: 7rem;
   }
 }
 @media screen and (max-width: 1024px) {
   .header_box {
-  .logo {
-    width: 103px;
+    .logo {
+      width: 103px;
+    }
   }
-}
 }
 </style>
